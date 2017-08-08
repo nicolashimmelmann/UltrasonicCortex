2a58bd5b979419cb772bf61923909c1d236e7005
fkaiser3 <felix.kaiser3@gmail.com>
2017-08-08 17:50:12 +0200

    Added Interrupt instead of polling

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 #include <stm32f030xc.h>
 using namespace chibios_rt;
 
+bool is_high_sensor1 = false;
+bool is_high_sensor2 = false;
+systime_t start1;
+systime_t end1;
+systime_t start2;
+systime_t end2;
+
+
 
 static SerialConfig sd5cfg = {
 		38400 // Baudrate
@@ -30,7 +38,12 @@ void initSensor() {
 	//GPIO_0 (15) Trigger
 	//GPIO_1 (14) Echo
 	palSetPadMode(GPIOB, 15, PAL_MODE_OUTPUT_PUSHPULL);
-	palSetPadMode(GPIOB, 14, PAL_MODE_INPUT_PULLUP);
+	palSetPadMode(GPIOB, 14, PAL_MODE_INPUT);
+
+	//GPIO_2 (6) Trigger
+	//GPIO_3 (5) Echo
+	palSetPadMode(GPIOB, 6, PAL_MODE_OUTPUT_PUSHPULL);
+	palSetPadMode(GPIOB, 5, PAL_MODE_INPUT);
 }
 
 uint16_t readValue() {
@@ -55,6 +68,60 @@ uint16_t readValue() {
 	}
 }
 
+
+static void sensor1(EXTDriver *extp, expchannel_t channel) {
+  (void)extp;
+  (void)channel;
+  chSysLockFromISR();
+  if(!is_high_sensor1)
+	 start1 = chVTGetSystemTimeX();
+  else
+	  end1 = chVTGetSystemTimeX();
+  is_high_sensor1 = !is_high_sensor1;
+  chSysUnlockFromISR();
+}
+
+static void sensor2(EXTDriver *extp, expchannel_t channel) {
+	  (void)extp;
+	  (void)channel;
+	  chSysLockFromISR();
+	  if(!is_high_sensor2)
+		 start2 = chVTGetSystemTimeX();
+	  else
+		  end2 = chVTGetSystemTimeX();
+	  is_high_sensor2 = !is_high_sensor2;
+	  chSysUnlockFromISR();
+}
+
+static const EXTConfig extcfg = {
+  {
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOB, sensor2},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOB, sensor1},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL}
+//    {EXT_CH_MODE_DISABLED, NULL},
+//    {EXT_CH_MODE_DISABLED, NULL},
+//    {EXT_CH_MODE_DISABLED, NULL}
+  }
+};
+
+
 int main(void) {
 	halInit();
 	chSysInit();
@@ -62,13 +129,34 @@ int main(void) {
 	initUART();
 	initSensor();
 	sdStart(&SD5, &sd5cfg);
+	bool test = false;
+	bool test2 = false;
+	start1 = 0;
+	end1 = 0;
+	start2 = 0;
+	end2 = 0;
 
-	NVIC_EnableIRQ(EXTI4_15_IRQn);
+	extStart(&EXTD1, &extcfg);
+	extChannelEnable(&EXTD1, 5);
+	extChannelEnable(&EXTD1, 14);
 
+	palSetPad(GPIOB, 15);
+	palSetPad(GPIOB, 6);
+	chThdSleepMicroseconds(100);
+	palClearPad(GPIOB, 15);
+	palClearPad(GPIOB, 6);
 	while(1) {
-		uint16_t duration = readValue();
-		writeSensorDataUART(duration);
-		chThdSleep(100);
+		if(start1 != 0 and end1 != 0 and !test) {
+			writeSensorDataUART(ST2US(end1 - start1));
+			test = !test;
+		}
+		if(start2 != 0 and end2 != 0 and !test2) {
+			writeSensorDataUART(ST2US(end2 - start2));
+			test2 = !test2;
+		}
+//		uint16_t duration = readValue();
+//		writeSensorDataUART(duration);
+//		chThdSleep(100);
 	}
 	return 0;
 }
