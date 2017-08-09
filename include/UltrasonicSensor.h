d6f47c177ff107b7416700d44073c0e464980402
fkaiser3 <felix.kaiser3@gmail.com>
2017-08-09 10:36:36 +0200

    Sensormessungen ausgelagert in eigene Klasse

--- include/UltrasonicSensor.h
+++ include/UltrasonicSensor.h
@@ -4,15 +4,49 @@
  *  Created on: Aug 8, 2017
  *      Author: felix
  */
+#include <hal.h>
 
 #ifndef ULTRASONICSENSOR_H_
 #define ULTRASONICSENSOR_H_
 
 class UltrasonicSensor {
 public:
-	UltrasonicSensor();
-	virtual ~UltrasonicSensor();
-	void getValue();
+	UltrasonicSensor(short triggerPin, short echoPin);
+	uint16_t getValue();
+private:
+	void interruptHandler(EXTDriver *extp, expchannel_t channel);
+	static bool extStarted = false;
+	bool isHighFlank = false;
+	bool finishedMeasurment = false;
+	systime_t startTime = 0;
+	systime_t endTime = 0;
+
+	static const EXTChannelConfig echoChannelConfig = {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOB, interruptHandler};
+	static const EXTConfig extcfg = {
+	  {
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL},
+	    {EXT_CH_MODE_DISABLED, NULL}
+	  }
+	};
+
 };
 
 #endif /* ULTRASONICSENSOR_H_ */
